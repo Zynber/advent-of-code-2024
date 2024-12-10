9ab5fb0957375d91ea7e1671f747f7fcde0974f6
Zynber <70592360+Zynber@users.noreply.github.com>
2024-12-10 19:25:53 +0800

    Day 10

---
+++ day_10/part_2.ts
@@ -0,0 +1,75 @@
+class Coords {
+  readonly x: number;
+  readonly y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  add(other: Coords) {
+    return new Coords(this.x + other.x, this.y + other.y);
+  }
+
+  access<T>(arr: T[][]): T {
+    return arr[this.y]?.[this.x];
+  }
+
+  safeAccess<T>(arr: T[][]): T | undefined {
+    return arr[this.y]?.[this.x];
+  }
+}
+
+const DIRECTIONS = [
+  new Coords(0, -1),
+  new Coords(1, 0),
+  new Coords(0, 1),
+  new Coords(-1, 0),
+] as const;
+
+function traverse(startPos: Coords): number {
+  let currentPos = startPos;
+  let currentHeight = currentPos.access(map);
+
+  while (currentHeight !== 9) {
+    const accessibleAdjacentCellPos = DIRECTIONS.map((dir) =>
+      currentPos.add(dir),
+    ).filter((checkPos) => checkPos.safeAccess(map) === currentHeight + 1);
+
+    switch (accessibleAdjacentCellPos.length) {
+      case 0:
+        return 0;
+
+      case 1:
+        currentPos = accessibleAdjacentCellPos[0];
+        currentHeight = currentPos.access(map);
+        break;
+
+      default:
+        return accessibleAdjacentCellPos
+          .map((pos) => traverse(pos))
+          .reduce((acc, curr) => acc + curr);
+    }
+  }
+
+  return 1;
+}
+
+const map = (await Bun.file(`${import.meta.dir}/input.txt`).text())
+  .split("\n")
+  .map((row) => row.split("").map((cell) => Number.parseInt(cell)));
+
+const width = map[0].length;
+const height = map.length;
+
+let sum = 0;
+
+for (let y = 0; y < height; y++) {
+  for (let x = 0; x < width; x++) {
+    if (map[y][x]) continue;
+
+    sum += traverse(new Coords(x, y));
+  }
+}
+
+console.log(sum);
